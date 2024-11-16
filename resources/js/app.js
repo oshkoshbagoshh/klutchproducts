9a58c3e10dfb4667a5ff051c8792b5b51481d46d
AJ Javadi <amirjavadi25@gmail.com>
2024-11-16 16:20:45 -0600

    got it working again.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,11 @@
-
 import './bootstrap';
+import '../css/app.css';
+
 
 import {createInertiaApp} from '@inertiajs/vue3';
 import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
 import {createApp, h} from 'vue';
 import {ZiggyVue} from 'ziggy-js';
-
 
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
