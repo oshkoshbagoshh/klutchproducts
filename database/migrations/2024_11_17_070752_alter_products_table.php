bea979761fcdec1586af62902d0aabf56d1cd044
AJ Javadi <amirjavadi25@gmail.com>
2024-11-17 03:27:21 -0600

    altering migrations

---
+++ database/migrations/2024_11_17_070752_alter_products_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // removing unnecessary fields from the products table, now that I have the final list of the product script
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
---
+++ zzMisc/backupdb.sh
@@ -0,0 +1,1 @@
+mysqldump -u root -p klutchproducts > klutch_products_backup.sql
